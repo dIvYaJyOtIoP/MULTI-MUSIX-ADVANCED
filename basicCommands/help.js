9a3aeeeb485b797acfde76fdb6b30507c97e0525
dIvYaJyOtIoP <139646426+dIvYaJyOtIoP@users.noreply.github.com>
2024-05-04 13:25:29 +0530

    Update help.js

--- basicCommands/help.js
+++ basicCommands/help.js
@@ -10,13 +10,7 @@ module.exports = {
     const botPing = Date.now() - message.createdTimestamp;
     const serverCount = message.client.guilds.cache.size;
     const embed = new EmbedBuilder()
-      .setColor('#2b71ec')
-      .setAuthor({
-        name: 'Im here to Help!',
-        iconURL: 'https://cdn.discordapp.com/attachments/1175487983915376662/1175667506791325706/communication.png?ex=656c10b0&is=65599bb0&hm=e378f1b355a2401bcab504b08a0766001d6b7c090c91ce0a7a7a87c868feb955&', 
-        url: 'https://discord.gg/FUEHs7RCqz'
-    })
-     
+      .setColor('#2b71ec')     
       .setDescription(`__**STATS :**__\n\n> **📊 Bot in servers:** ${serverCount}\n> **🟢 Bot Ping:** ${botPing}ms\n> **👑 Made By [GlaceYT](https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A)**\n\n__**COMMANDS :**__ `)
       .addFields(
         // Basic commands category
