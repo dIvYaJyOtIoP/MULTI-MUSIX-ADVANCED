348e3625651f094038f74bf83540d61e020fc244
dIvYaJyOtIoP <139646426+dIvYaJyOtIoP@users.noreply.github.com>
2024-05-04 14:38:27 +0530

    Update invite.js

--- basicCommands/invite.js
+++ basicCommands/invite.js
@@ -9,7 +9,6 @@ module.exports = {
             .setColor('#FFFFFF')
             .setTitle('Invite Me')
             .setDescription(`[Click here to invite me](${supportServerLink})`)
-            .setThumbnail(`https://images-ext-1.discordapp.net/external/rJT8V1XxxrF27NkqhADaBNs_yJOz9NhU5wQoV_O916I/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/1219975767056715797/a4260652b63751e3aac1d415962842c4.webp?format=webp&width=570&height=570`)
             .setTimestamp();
 
 
