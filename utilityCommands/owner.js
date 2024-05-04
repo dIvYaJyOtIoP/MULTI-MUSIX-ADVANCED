ea3aa2468dfd06e469d71bb35c747971b8f8efba
dIvYaJyOtIoP <139646426+dIvYaJyOtIoP@users.noreply.github.com>
2024-05-04 14:13:42 +0530

    Update owner.js

--- utilityCommands/owner.js
+++ utilityCommands/owner.js
@@ -4,12 +4,12 @@ module.exports = {
     name: 'owner',
     description: 'Bot owner info',
     execute(message, args) {
-        const youtubeLink = 'https://discord.gg/FUEHs7RCqz';
-        const InstagramLink = 'https://discord.gg/FUEHs7RCqz';
+        const youtubeLink = 'https://youtube.com/@djcodez';
+        const InstagramLink = 'https://instagram.com/ig_djcodes';
         const embed = new EmbedBuilder()
             .setColor('#FFFFFF')
-            .setTitle(' 🫅 Owner Info')
-            .setDescription(`__**About me**__:\n 🤖 Myself Shiva aka RTX. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n ❤️ [RTX GAMING](${youtubeLink})\n 💙 [Atstreak](${InstagramLink})`)
+            .setTitle('<a:6968blackcrown:1236208075451666442> OWNER INFO')
+            .setDescription(`__**About me**__:\n 🤖 Myself DivyaJyoti aka DJCodez. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n ❤️ [DJ CODEZ](${youtubeLink})\n 💙 [DJCODEZ](${InstagramLink})`)
             .setTimestamp();
 
 
